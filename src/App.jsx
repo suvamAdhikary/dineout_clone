fcd9938a02364eb9c15f5c7af8f9c5af6b11ee51
dharmeshrao <yadavdydv@gmail.com>
2021-12-15 01:35:43 +0530

    signup done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ConfirmationPage } from './Components/Header/ConfirmationPage';
 import { Navbar } from './Components/Header/Navbar';
 import { ScrollNavbar } from './Components/Header/ScrollNavbar';
 import { Popup } from './Components/Pages/Home/Popup';
+import { Signup } from './Components/Pages/Signup/Signup';
 import { GlobalStyles } from './Components/Styles/Global';
 import Dineout from './Dineout';
 
@@ -10,7 +11,8 @@ function App() {
   return (<>
     <GlobalStyles />
     <div className="App">
-      <Navbar/>
+      {/* <Navbar/> */}
+      <Signup/>
       {/* <ScrollNavbar/> */}
       {/* <ConfirmationPage/> */}
       {/* <Popup/> */}
