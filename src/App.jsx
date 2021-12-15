8548ec0115c52d1af85ccecbaeae107ee45cde24
dharmeshrao <yadavdydv@gmail.com>
2021-12-15 10:54:17 +0530

    signin done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 
-import { ConfirmationPage } from './Components/Header/ConfirmationPage';
-import { Navbar } from './Components/Header/Navbar';
-import { ScrollNavbar } from './Components/Header/ScrollNavbar';
-import { Popup } from './Components/Pages/Home/Popup';
-import { Signup } from './Components/Pages/Signup/Signup';
+// import { ConfirmationPage } from './Components/Header/ConfirmationPage';
+// import { Navbar } from './Components/Header/Navbar';
+// import { ScrollNavbar } from './Components/Header/ScrollNavbar';
+// import { Popup } from './Components/Pages/Home/Popup';
+// import { Signin } from './Components/Pages/signin/Signin';
+// import { Signup } from './Components/Pages/Signup/Signup';
 import { GlobalStyles } from './Components/Styles/Global';
 import Dineout from './Dineout';
 
@@ -12,10 +13,11 @@ function App() {
     <GlobalStyles />
     <div className="App">
       {/* <Navbar/> */}
-      <Signup/>
+      {/* <Signup/> */}
       {/* <ScrollNavbar/> */}
       {/* <ConfirmationPage/> */}
       {/* <Popup/> */}
+      {/* <Signin/> */}
       <Dineout />
     </div>
   </>);
