eb95d99e8b58448726d8aabb53cce6652f208d2d
dharmeshrao <yadavdydv@gmail.com>
2021-12-15 15:41:20 +0530

    otp page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 
-// import { ConfirmationPage } from './Components/Header/ConfirmationPage';
-// import { Navbar } from './Components/Header/Navbar';
-// import { ScrollNavbar } from './Components/Header/ScrollNavbar';
-// import { Popup } from './Components/Pages/Home/Popup';
-// import { Signin } from './Components/Pages/signin/Signin';
-// import { Signup } from './Components/Pages/Signup/Signup';
+import { ConfirmationPage } from './Components/Header/ConfirmationPage';
+import { Navbar } from './Components/Header/Navbar';
+import { ScrollNavbar } from './Components/Header/ScrollNavbar';
+import { Popup } from './Components/Pages/Home/Popup';
+import { Signin } from './Components/Pages/signin/Signin';
+import { Signup } from './Components/Pages/Signup/Signup';
+import { VerifyOtp } from './Components/Header/VerifyOtp';
 import { GlobalStyles } from './Components/Styles/Global';
 import Dineout from './Dineout';
 
@@ -18,6 +19,7 @@ function App() {
       {/* <ConfirmationPage/> */}
       {/* <Popup/> */}
       {/* <Signin/> */}
+      <VerifyOtp/>
       <Dineout />
     </div>
   </>);
