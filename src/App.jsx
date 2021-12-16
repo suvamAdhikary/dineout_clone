1e7778a6e619ae13374443c9660a459bffceb72e
dharmeshrao <yadavdydv@gmail.com>
2021-12-16 14:21:27 +0530

    otp done

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@
 import { FoodCard } from './Components/Pages/Home/FoodCard';
 import { PetFriendly } from './Components/Pages/PetFriendly/PetFriendly';
 import { GlobalStyles } from './Components/Styles/Global';
+import {Signin} from './Components/Pages/signin/Signin'
 import Dineout from './Dineout';
+import { Explore } from './Components/Pages/Home/Explore';
 function App() {
   return (<>
     <GlobalStyles />
     <div className="App">
       {/* <FoodCard/> */}
-      <PetFriendly/>
+      {/* <PetFriendly/> */}
+      {/* <Signin/> */}
+      <Explore/>
       <Dineout />
     </div>
   </>);
---
+++ src/Utils/Firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCGnqOaqMxyi-RJGataTmYNsqVcnueHZBQ",
+  authDomain: "dineout-client-7c634.firebaseapp.com",
+  projectId: "dineout-client-7c634",
+  storageBucket: "dineout-client-7c634.appspot.com",
+  messagingSenderId: "753726811097",
+  appId: "1:753726811097:web:c27404b7a6a90c8d2f47c8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
