f9944eee5306f121ee5997f85799dd6c40570e10
Suvam Adhikary <adhikary.saheb408@gmail.com>
2021-12-17 12:46:39 +0530

    refactor codebase, favicon

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
 import { SigninContextProvider } from "./Context/SignInContext";
 
@@ -16,8 +14,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
