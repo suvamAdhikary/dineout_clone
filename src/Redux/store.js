f604dea17341cbbad4d6ef8fe720f0dcf2e69eb1
Suvam Adhikary <adhikary.saheb408@gmail.com>
2021-12-18 13:30:26 +0530

    redux

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,4 +1,5 @@
 import { applyMiddleware, combineReducers, compose, createStore } from "redux";
+import thunk from "redux-thunk";
 import { restaurantReducer } from "./Restaurants/reducer";
 
 
@@ -9,12 +10,25 @@ const rootReducer = combineReducers({
 })
 
 
+const composeEnhancers = (
+
+    typeof window !== "undefined" && window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__
+
+) || compose;
+
+const enhancer = composeEnhancers(applyMiddleware(
+
+    thunk
+));
+
+
 
 
 export const store = createStore(
 
     rootReducer,
 
+    enhancer,
 
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
