45e4c99093d611e66d35903be7062777888d5a62
Suvam Adhikary <suvamadhikary@Suvams-MacBook-Pro.local>
2023-04-18 14:16:03 +0530

    changed server heroku to render

--- src/Utils/Axios.jsx
+++ src/Utils/Axios.jsx
@@ -2,25 +2,25 @@ import axios from "axios";
 
 export const getAllRestaurants = () => {
 
-    return axios.get("https://dineout-clone.herokuapp.com/restaurants");
+    return axios.get("https://dineout-clone-server.onrender.com/restaurants");
 }
 
 export const addNewUser = ( payload ) => {
-    return axios.post("https://dineout-clone.herokuapp.com/users", payload);
+    return axios.post("https://dineout-clone-server.onrender.com/users", payload);
 }
 
 export const getAUser = ( id ) => {
-    return axios.get(`https://dineout-clone.herokuapp.com/users/${id}`);
+    return axios.get(`https://dineout-clone-server.onrender.com/users/${id}`);
 }
 
 export const getARestaurant = ( id ) => {
-    return axios.get(`https://dineout-clone.herokuapp.com/restaurants/${id}`);
+    return axios.get(`https://dineout-clone-server.onrender.com/restaurants/${id}`);
 }
 
 export const getUserByMobile = (mobile) => {
-    return axios.get(`https://dineout-clone.herokuapp.com/users/mobiles/${mobile}`);
+    return axios.get(`https://dineout-clone-server.onrender.com/users/mobiles/${mobile}`);
 }
 
 export const updateUser = (id, payload) => {
-    return axios.patch(`https://dineout-clone.herokuapp.com/users/${id}`, payload);
+    return axios.patch(`https://dineout-clone-server.onrender.com/users/${id}`, payload);
 } 
